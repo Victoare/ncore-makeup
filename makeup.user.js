089a67ab47a35b52838346e783c6aa23006df421
Victoare <victoare@gmail.com>
2022-05-13 13:49:11 +0200

    imdb id érzékelési gondok javítása

--- makeup.user.js
+++ makeup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nCore - makeup
 // @namespace    https://github.com/Victoare/ncore-makeup
-// @version      0.5.1
+// @version      0.5.2
 // @description  Ncore púder és szájfény
 // @author       Victoare
 // @match        https://ncore.pro/torrents.php*
@@ -93,7 +93,8 @@
 
   var getImdbID = function ($row) {
     var attr = $row.find('.infolink').attr('href');
-    return attr ? attr.match(/\/(tt[0-9]*)/)[1] : '';
+    var match = attr ? attr.match(/\/(ev\d{7}\/\d{4}(-\d)?|(ch|co|ev|nm|tt)\d{7})/i) : [];
+    return match.length>0 ? match[1] : '';
   }
   var getCoverImg = function ($row) {
     var attr = $row.find('img.infobar_ico').attr('onmouseover');
